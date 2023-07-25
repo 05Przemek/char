912b513f9fa6652040e1c1cbaa9fa1c42673539d
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 14:41:56 +0200

    char git

---
+++ char/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace String2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            char przykladowyznak;
+
+            przykladowyznak = '\'';
+        }
+    }
+}
